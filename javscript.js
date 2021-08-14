5408e7f084b1693f3957d7960c62f770db24c098
ssterling27 <samuel.steinberg27@gmail.com>
2021-08-13 22:19:30 -0700

    finalized everything

---
+++ javscript.js
@@ -0,0 +1,20 @@
+// const allNumbers = () => {
+//   let characterInput = document.getElementById('characterInput').value
+//   for (let i = 0; i <= characterInput; i++) {
+//     let digit = Math.floor(Math.random() * 9)
+//     console.log(digit)
+//   }
+// }
+
+// const num = 13;
+// const randomString = (len = 1) => {
+//   const charSet =
+//     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+//   let randomString = '';
+//   for (let i = 0; i < len; i++) {
+//     let randomPoz = Math.floor(Math.random() * charSet.length);
+//     randomString += charSet.substring(randomPoz, randomPoz + 1);
+//   };
+//   return randomString;
+// };
+// console.log(randomString(num));
